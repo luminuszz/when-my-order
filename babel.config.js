25938c4b76b9c70c1cbdbcdfeaba1dbd2204fdf0
daviribeiro <luminuszz43@gmail.com>
2023-02-12 13:22:18 -0300

    feat: initial commit and config eslint prettier and typescript

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,29 @@
-module.exports = function(api) {
+module.exports = (api) => {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        require.resolve("babel-plugin-module-resolver"),
+
+        {
+          root: ["."],
+          extensions: [".ts", ".tsx", ".js", ".ios.js", ".android.js", ".json"],
+          alias: {
+            "@app": "./src",
+            "@core": "./src/core",
+          },
+        },
+      ],
+
+      [
+        "module:react-native-dotenv",
+        {
+          path: "./.env.local",
+          safe: true,
+          moduleName: "@env",
+        },
+      ],
+    ],
   };
 };
